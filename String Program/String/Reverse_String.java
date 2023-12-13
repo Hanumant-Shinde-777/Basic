7447b328742584141ef81be3f91dc00a6eb53a30
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-12-13 23:17:30 +0530

    Using inbuild method

--- String Program/String/Reverse_String.java
+++ String Program/String/Reverse_String.java
@@ -1,16 +1,27 @@
 
 public class Reverse_String {
     public static void reverse(String str) {
-        String output = "";
-        for (int i = str.length() - 1; i >= 0; i--) {
-            output = output + str.charAt(i);
-        }
-        System.out.println(output);
+        // String output = "";
+        // for (int i = str.length() - 1; i >= 0; i--) {
+        //     output = output + str.charAt(i);
+        // }
+        // System.out.println(output);
+
+        StringBuffer sb = new StringBuffer(str);
+        sb.reverse();
+        System.out.println(sb);
+        
+            //using the string builder
+
+            StringBuilder builder = new StringBuilder(str);
+            builder.reverse();
+            System.out.println(builder);
     }
+
 
     public static void main(String[] args) {
         String str = "Hello World";
         reverse(str);
-        System.out.println(str);
+        // System.out.println(str);
     }
 }

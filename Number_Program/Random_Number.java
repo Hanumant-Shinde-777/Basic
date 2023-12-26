d2ded6a80bf0ba503ab2e30752dd166416829af5
Hanumant-Shinde-777 <hanumantshinde1213@gmail.com>
2023-12-26 23:27:21 +0530

    Random number using Math class

---
+++ Number_Program/Random_Number.java
@@ -0,0 +1,10 @@
+public class Random_Number {
+    public static void main(String args[])   
+    {   
+    // Generating random numbers  
+    System.out.println("1st Random Number: " + (int) Math.random() * 10 +1);   
+    System.out.println("2nd Random Number: " +  (int)Math.random() * 10 +1);  
+    System.out.println("3rd Random Number: " +  Math.random() * 10 +1);   
+    System.out.println("4th Random Number: " + Math.random() * 10 +1);   
+    }   
+}

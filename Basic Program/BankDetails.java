c97fcff1bec70cf1bb8eaba86b5d88ad14f77890
Hanumant Shinde <90370971+Hanumant-Shinde-777@users.noreply.github.com>
2023-12-27 21:29:33 +0530

    Update BankDetails.java

--- Basic Program/BankDetails.java
+++ Basic Program/BankDetails.java
@@ -1,5 +1,5 @@
 public class BankDetails {
-
+//Created method of Bank Details
 
     public static void AccountInformation(String AccountType, int AccountLimit, String AccountName){
         System.out.println("Accoun type is"+ AccountType +" Account Limition"+  AccountLimit+" Account holder name"+AccountLimit);

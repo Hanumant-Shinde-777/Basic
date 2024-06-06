a7f7f57eb289f1d828a2d8969a60d6d670a27598
Hanumant Shinde <90370971+Hanumant-Shinde-777@users.noreply.github.com>
2024-06-06 23:52:08 +0530

    Update Pattern36.java

--- Pattern36.java
+++ Pattern36.java
@@ -2,13 +2,17 @@
 // import yhe package 
 import java.util.*;
 public class Pattern36 {
+
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
             System.out.print("*\t");
             }
+            
             System.out.println(" ");
         }
     }
